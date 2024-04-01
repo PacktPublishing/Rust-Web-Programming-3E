f1e6da1c2b9b656bbf1ca158f9d4a8840dbfb69a
maxwellflitton <maxwellflitton@gmail.com>
2024-04-01 23:49:23 +0100

    finishing header extraction

---
+++ chapter05/header_extraction/nanoservices/to_do/networking/actix_server/src/api/basic_actions/create.rs
@@ -0,0 +1,20 @@
+use core::api::basic_actions::{
+    create::create as create_core,
+    get::get_all as get_all_core
+};
+use core::structs::ToDoItem;
+use glue::{
+    errors::NanoServiceError,
+    token::HeaderToken
+};
+use actix_web::{
+    HttpResponse,
+    web::Json
+};
+
+
+pub async fn create(token: HeaderToken, body: Json<ToDoItem>) -> Result<HttpResponse, NanoServiceError> {
+    println!("Token: {}", token.message);
+    let _ = create_core(body.into_inner()).await?;
+    Ok(HttpResponse::Ok().json(get_all_core().await?))
+}
