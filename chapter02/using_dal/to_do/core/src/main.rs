11e94914b941a31086fd13010a43f247a111bc3c
maxwellflitton <maxwellflitton@gmail.com>
2024-05-31 10:12:19 +0100

    adding clap to chapter 2

--- chapter02/using_dal/to_do/core/src/main.rs
+++ chapter02/using_dal/to_do/core/src/main.rs
@@ -1,21 +1,29 @@
 mod enums;
 mod structs;
 mod api;
-
 use api::basic_actions::create::create;
-use std::env;
-
 use crate::enums::TaskStatus;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    title: String,
+    #[arg(short, long)]
+    status: String,
+}
 
 
 fn main() -> Result<(), String> {
-    let args: Vec<String> = env::args().collect();
-    let title: &String = &args[1];
-    let status: TaskStatus = enums::TaskStatus::from_string(&args[2])?;
-
+    let args = Args::parse();
+    let status_enum = TaskStatus::from_string(
+        &args.status
+    )?;
     let to_do_item = create(
-        title, 
-        status
+        &args.title, 
+        status_enum
     )?;
     println!("{}", to_do_item);
     Ok(())
