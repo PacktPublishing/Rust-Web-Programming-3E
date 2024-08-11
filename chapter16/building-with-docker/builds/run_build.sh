b6df46c16dcfa132832d39193191e173d6b97deb
maxwellflitton <maxwellflitton@gmail.com>
2024-08-12 00:33:55 +0100

    deployment is working

---
+++ chapter16/building-with-docker/builds/run_build.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+# builds/run_build.sh
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH
+
+terraform init
+terraform apply -auto-approve
+terraform output -json > ./output.json
+
+IP_ADDRESS=$(jq --raw-output '.ec2_global_ips.value[0][0]' \
+output.json)
+echo $IP_ADDRESS
+
+echo "waiting for server to be built"
+sleep 5
+echo "attempting to enter server"
+
+scp -i "~/.ssh/keys/remotebuild.pem" \
+-o StrictHostKeyChecking=no \
+-r ../nanoservices \
+ec2-user@$IP_ADDRESS:/home/ec2-user/nanoservices
+
+rm -rf ../ingress/frontend/node_modules
+
+scp -i "~/.ssh/keys/remotebuild.pem" \
+-o StrictHostKeyChecking=no \
+-r ../ingress \
+ec2-user@$IP_ADDRESS:/home/ec2-user/ingress
+
+scp -i "~/.ssh/keys/remotebuild.pem" \
+-o StrictHostKeyChecking=no \
+-r ../glue \
+ec2-user@$IP_ADDRESS:/home/ec2-user/glue
+
+scp -i "~/.ssh/keys/remotebuild.pem" \
+-o StrictHostKeyChecking=no \
+../docker-compose.yml \
+ec2-user@$IP_ADDRESS:/home/ec2-user/
+
+scp -i "~/.ssh/keys/remotebuild.pem" \
+-o StrictHostKeyChecking=no \
+../.env \
+ec2-user@$IP_ADDRESS:/home/ec2-user/
+
+scp -i "~/.ssh/keys/remotebuild.pem" \
+-o StrictHostKeyChecking=no \
+../Cargo.toml \
+ec2-user@$IP_ADDRESS:/home/ec2-user/
+
+scp -i "~/.ssh/keys/remotebuild.pem" \
+-o StrictHostKeyChecking=no \
+../Dockerfile \
+ec2-user@$IP_ADDRESS:/home/ec2-user/
+
+ssh -i "~/.ssh/keys/remotebuild.pem" \
+-o StrictHostKeyChecking=no -t \
+ec2-user@$IP_ADDRESS << EOF
+    until [ -f ./output.txt ]
+    do
+        echo "waiting for file"
+        sleep 3
+    done
+    echo "File found"
+    sudo curl https://sh.rustup.rs -sSf | bash -s -- -y
+    source ~/.cargo/env
+    export $(cat .env | xargs)
+    cd ingress
+    cd frontend
+    npm install
+    npm run build
+    cd ..
+    cargo build --release
+EOF
---
+++ chapter16/deploying-onto-server/builds/main.tf
@@ -0,0 +1,28 @@
+# builds/main.tf
+terraform {
+    required_version = ">= 1.9.4"
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.62.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "build_server" {
+    ami = "ami-05ea2888c91c97ca7"
+    instance_type = "t2.medium"
+    key_name = "remotebuild"
+    user_data = file("server_build.sh")
+    tags = {
+    Name = "to-do build server"
+    }
+}
+
+output "ec2_global_ips" {
+    value = ["${aws_instance.build_server.*.public_ip}"]
+}
