830b63977f77302c970a61f6e6c0c1e34894d249
maxwellflitton <maxwellflitton@gmail.com>
2024-11-18 04:56:01 +0000

    CORs now fully defined

--- chapter07/inserting-styles-with-css/alternative-ingresses/rocket-ingress/src/main.rs
+++ chapter07/inserting-styles-with-css/alternative-ingresses/rocket-ingress/src/main.rs
@@ -104,12 +104,21 @@ impl Fairing for CORS {
             kind: Kind::Response
         }
     }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    async fn on_response<'r>(
+        &self, _request: &'r Request<'_>, response: &mut Response<'r>
+    ) {
+        response.set_header(Header::new(
+            "Access-Control-Allow-Origin", "*"
+        ));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"
+        ));
+        response.set_header(
+            Header::new("Access-Control-Allow-Headers", "*"
+        ));
+        response.set_header(
+            Header::new("Access-Control-Allow-Credentials", "true"
+        ));
     }
 }
 
